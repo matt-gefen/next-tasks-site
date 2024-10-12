18ea80685bb7a9bd7bd8b82bc4e0f322b64a1d24
Matt Gefen <matt.gefen@gmail.com>
2024-10-11 20:12:43 -0600

    adding some basic data storage using browser storage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+import { useEffect, useLayoutEffect, useState } from "react";
 import styles from "./page.module.css";
 
 // requirements
@@ -8,12 +10,41 @@ import styles from "./page.module.css";
 // Remove tasks using a delete button
 // Task list should persist between page reloads (use browser localStorage)
 
+interface ISession {
+  name: string,
+  tasks: []
+}
+
 export default function Home() {
+
+  const [session, setSession] = useState<ISession>({
+    name: "saved_tasks",
+    tasks: []
+  })
+
+  useLayoutEffect(()=> {
+    if (!localStorage.getItem("session")) {
+      localStorage.setItem("session", JSON.stringify(session))
+    }
+    else {
+      const localSessionString = localStorage.getItem("session")
+      if (typeof localSessionString === 'string') {
+        let localSessionObject = JSON.parse(localSessionString)
+        setSession(localSessionObject)
+      }
+    }
+  }, [])
+
+  useEffect(()=> {
+    console.log(session)
+  }, [session])
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <div>
-          Test Page
+          Add Something to tasks for proof of concept
+          <button onClick={()=> {}}>Click me</button>
         </div>
       </main>
     </div>
